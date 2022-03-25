8353826bb1904fced41540ce94f7a7319b89b6f9
Grigory Latyshev <thegriglat@gmail.com>
2022-03-25 11:57:37 +0300

    Use Angular Material

--- apps/urlshortener/src/app/app.module.ts
+++ apps/urlshortener/src/app/app.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,18 +18,37 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RedirectComponent } from './redirect/redirect.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: ':shorturl', component: RedirectComponent }
+  { path: ':shorturl', component: RedirectComponent },
 ];
 
-
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, RedirectComponent],
-  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    RedirectComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatDividerModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppModule {}
--- apps/urlshortener/src/app/home/home.component.ts
+++ apps/urlshortener/src/app/home/home.component.ts
@@ -3,6 +3,13 @@ import { ShortenService } from '../shorten.service';
 
 import isURL from 'validator/lib/isURL';
 import { Observable } from 'rxjs';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+} from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'evolving-home',
@@ -10,31 +17,27 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  url: string = '';
+  private urlValidator: ValidatorFn = (control: AbstractControl) => {
+    return isURL(control.value) ? null : { invalidUrl: true };
+  };
+
+  formGroup = new FormGroup({
+    url: new FormControl('', [Validators.required, this.urlValidator]),
+  });
+
   shorturl: string = '';
 
   updated = false;
-
-  public isUrlCorrect(url: string): boolean {
-    return isURL(url);
-  }
 
   constructor(private shortService: ShortenService) {}
 
   ngOnInit(): void {}
 
-  updateUrl(event: Event): void {
-    this.url = (event.target as HTMLInputElement).value;
-    this.updated = false;
-  }
-
   shortify(): void {
     this.updated = false;
-    this.shortService
-      .getShortURL(this.url)
-      .subscribe((shortUrl) => {
-        this.shorturl = shortUrl;
-        this.updated = true;
-      });
+    this.shortService.getShortURL(this.formGroup.get("url")?.value).subscribe((shortUrl) => {
+      this.shorturl = shortUrl;
+      this.updated = true;
+    });
   }
 }
