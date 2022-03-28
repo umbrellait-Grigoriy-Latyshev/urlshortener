91d05546f1c834e7bccbd028de25c94d139cbc2e
Grigory Latyshev <thegriglat@gmail.com>
2022-03-28 11:39:17 +0300

    Add @nestjs/config, add .env file
    Setup api and docker compose for .env

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Url } from './url.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../../../.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: '****',
-      password: '****',
-      database: 'test',
+      host: process.env.DBHOST,
+      port: parseInt(process.env.DBPORT),
+      username: process.env.DBUSERNAME,
+      password: process.env.DBPASSWORD,
+      database: process.env.DBNAME,
       entities: [Url],
     }),
     TypeOrmModule.forFeature([Url]),
