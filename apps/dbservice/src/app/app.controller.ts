b7d6a11bbd2ae8e9c1c6490477175ab108258457
Grigory Latyshev <thegriglat@gmail.com>
2022-03-28 17:17:25 +0300

    Fix this issue

--- apps/dbservice/src/app/app.controller.ts
+++ apps/dbservice/src/app/app.controller.ts
@@ -2,28 +2,27 @@ import { Controller } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-import {Console, ConsoleService} from 'nestjs-console';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Url } from '@evolving/api-interfaces';
+import { Console, ConsoleService } from 'nestjs-console';
 
 @Controller()
 @Console()
 export class AppController {
   constructor(private appService: AppService,
-    private consoleService: ConsoleService,
-    @InjectRepository(Url)
-    private urlRepository: Repository<Url>) {
-      
-      const cli = this.consoleService.getCli();
-      this.consoleService.createCommand(
-        {
-            command: 'clean <days>',
-            description: 'remove all rows older than <days>'
-        },
-        this.appService.clean(this.urlRepository),
-        cli // attach the command to the cli
+    private consoleService: ConsoleService) {
+
+    const cli = this.consoleService.getCli();
+    this.consoleService.createCommand(
+      {
+        command: 'clean <days>',
+        description: 'remove all rows older than <days>'
+      },
+      {
+        instance: this.appService,
+        methodName: "clean"
+      },
+      // this.appService.clean,
+      cli // attach the command to the cli
     );
-    }
+  }
 
 }
--- apps/dbservice/src/app/app.service.ts
+++ apps/dbservice/src/app/app.service.ts
@@ -1,26 +1,26 @@
 import { Url } from '@evolving/api-interfaces';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 
 @Injectable()
 export class AppService {
 
-  public clean(repo: Repository<Url>) {
-    return async (days: number) => {
-      if (days < 0)
-        days = -days;
-      console.log(`Called clean(${days})`);
-      const today = new Date(Date.now());
-      const urls = await repo.createQueryBuilder("url")
-        .where("date_part('day', :today - url.createdAt) >= :days", { today: today, days: days })
-        .getMany();
+  constructor(@InjectRepository(Url) private repo: Repository<Url>) { }
 
-      console.log(`Asking for remove the following urls (${urls.length}):`)
-      urls.forEach(e => console.log(JSON.stringify(e)));
-      await repo.remove(urls);
-      console.log("Removed.")
-    }
+  public async clean(days: number) {
+    if (days < 0)
+      days = -days;
+    console.log(`Called clean(${days})`);
+    const today = new Date(Date.now());
+    const urls = await this.repo.createQueryBuilder("url")
+      .where("date_part('day', :today - url.createdAt) >= :days", { today: today, days: days })
+      .getMany();
+    console.log(`Asking for remove the following urls (${urls.length}):`)
+    urls.forEach(e => console.log(JSON.stringify(e)));
+    await this.repo.remove(urls);
+    console.log("Removed.")
   }
 
 
