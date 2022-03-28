afe29e978934db9611e4e233ddae94aaf364ed0f
Grigory Latyshev <thegriglat@gmail.com>
2022-03-28 11:37:30 +0300

    Add Logging to api

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -6,6 +6,8 @@ import {
   URLMessage,
 } from '@evolving/api-interfaces';
 
+import { Logger } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
 @Controller()
@@ -16,20 +18,23 @@ export class AppController {
   async getShortUrl(
     @Body() params: CreateShortURLMessage
   ): Promise<URLMessage> {
-    let url = await this.appService.getShortUrl(params.url, params.suggested);
+    Logger.log(`Requested /short api:\n  ${JSON.stringify(params)}`);
+    const url = await this.appService.getShortUrl(params.url, params.suggested);
     return { url: url, success: true };
   }
 
   @Get('long/:url')
   async getLongUrl(@Param() params): Promise<URLMessage> {
-    let url = await this.appService.getFullUrl(params.url);
+    Logger.log(`Requested /long/${params.url}`);
+    const url = await this.appService.getFullUrl(params.url);
     if (url.length === 0) return { url: '', success: false };
     return { url: url, success: true };
   }
 
   @Get('/available/:url')
   async getShortUrlAvailability(@Param() params): Promise<StatusMessage> {
-    let available = await this.appService.isAvailable(params.url);
+    Logger.log(`Requested /available/${params.url}`);
+    const available = await this.appService.isAvailable(params.url);
     return { success: available };
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,8 @@ import { createHmac } from 'crypto';
 import { Repository } from 'typeorm';
 import { Url } from './url.entity';
 
+import {Logger} from '@nestjs/common';
+
 @Injectable()
 export class AppService {
   constructor(
@@ -37,20 +39,25 @@ export class AppService {
       _hash2 = this.getRandomHash(6);
     } while (row);
     const newentry: Url = new Url(_hash, url);
+    Logger.log(`Insert to db value ${JSON.stringify(newentry)}`);
     await this.urlRepository.insert(newentry);
     return newentry.shorturl;
   }
 
   async getFullUrl(url: string): Promise<string> {
-    let row = await this.urlRepository.findOne({ where: { shorturl: url } });
-    if (!row) return ''; // TODO: error propagation
+    const row = await this.urlRepository.findOne({ where: { shorturl: url } });
+    if (!row){
+      Logger.log(`Cannot find row for ${url} in db, return ''`);
+      return '';
+    }; // TODO: error propagation
     row.count++;
+    Logger.log(`Increase count for row ${JSON.stringify(row)}, saving to db`);
     await this.urlRepository.save(row);
     return row.fullurl;
   }
 
   async isValid(shorturl: string): Promise<boolean> {
-    let row = await this.urlRepository.findOne({
+    const row = await this.urlRepository.findOne({
       where: { shorturl: shorturl },
     });
     return row !== undefined;
